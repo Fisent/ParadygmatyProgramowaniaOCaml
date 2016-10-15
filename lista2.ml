8b838582cebeca9880a0b8f336946ae6a2404498
Fisent <lukzieniewicz@gmail.com>
2016-10-15 16:43:15 +0200

    lista2 prawie zrobiona

--- lista2.ml
+++ lista2.ml
@@ -10,17 +10,16 @@ let rec fib(n, prev, next) = match n with
 | 1 -> next
 | _ -> fib((n-1), next, (next+prev));;
 
+let rec initSegment(segment, list) = 
+	match(segment, list) with
+	|	([], _) -> true
+	| (hd::tl, hd2::tl2) when hd = hd2 -> initSegment(tl, tl2)
+	| (_, _) -> false;;
+
 
 let rec replaceNth(list, n, obj) = 
 	if list = [] then raise(Failure "brak listy")
 	else if n = 0 then obj :: List.tl list
 	else List.hd list :: replaceNth(List.tl list, n-1, obj);;
-
-replaceNth (['o';'l';'a'] ,1,'s')
-
-replaceNth([1,2,3], 1, "a");;
-
-
-
 
 
